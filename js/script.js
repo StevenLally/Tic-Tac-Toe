b0db18bfbb0b79fced089d0a4e407c0cfb96dd6f
Steve Lally <steve.lally89@gmail.com>
2020-11-11 10:16:48 -0500

    Game Over Condition & Reset

--- js/script.js
+++ js/script.js
@@ -2,27 +2,23 @@
 const gameBoard = (() => {
     let moveCount = 0;
     let currentPlayer = "X";
+    let gameOver = false;
     const board = document.querySelectorAll(".board > .row > div");
 
     const initializeBoard = (boardNodes) => {
         boardNodes.forEach(node => {
             node.addEventListener("click", () => {
-                moveCount++;
-                node.innerHTML = currentPlayer;
-                updateBoardState(node.id, currentPlayer);
-                if (currentPlayer === "X") {
-                    currentPlayer = "O";
-                } else {
-                    currentPlayer = "X";
-                }
-                
-                const result = checkWin(boardState);
-                if (result) {
-                    if (result === "Tie"){
-                        alert("It's a Tie!");
+                if (!gameOver){
+                    moveCount++;
+                    node.innerHTML = currentPlayer;
+                    updateBoardState(node.id, currentPlayer);
+                    if (currentPlayer === "X") {
+                        currentPlayer = "O";
                     } else {
-                        alert(result + "Wins!");
+                        currentPlayer = "X";
                     }
+                    
+                    setTimeout(() => checkWin(boardState), 500);
                 }
             });
         });
@@ -55,52 +51,68 @@ const gameBoard = (() => {
         const botL = boardArr[6].value;
         const botM = boardArr[7].value;
         const botR = boardArr[8].value;
+        let winner = null;
 
         if (topL === topM && topL === topR) {
             if (topL != null){
-                return topL;
+                winner = topL;
             }
         }
         if (midL === midM && midL === midR) {
             if (midL != null){
-                return midL;
+                winner = midL;
             }
         }
         if (botL === botM && botL === botR) {
             if (botL != null){
-                return botL;
+                winner = botL;
             }
         }
         if (topL === midL && topL === botL) {
             if (topL != null){
-                return topL;
+                winner = topL;
             }
         }
         if (topM === midM && topM === botM) {
             if (topM != null){
-                return topM;
+                winner = topM;
             }
         }
         if (topR === midR && topR === botR) {
             if (topR != null){
-                return topR;
+                winner = topR;
             }
         }
         if (topL === midM && topL === botR) {
             if (topL != null){
-                return topL;
+                winner = topL;
             }
         }
         if (topR === midM && topR === botL) {
             if (topR != null){
-                return topR;
+                winner = topR;
             }
         }
-        if (moveCount === 9){
-            return "Tie";
-        } else {
-            return false;
-        }
+
+        if (winner != null){
+            alert(winner + " Wins!");
+            gameOver = true;
+        } else if (moveCount === 9){
+            alert("It's a Tie!");
+            gameOver = true;
+        } 
+    };
+
+    const resetBoard = () => {
+        moveCount = 0;
+        currentPlayer = "X";
+        gameOver = false;
+        boardState.forEach(obj => {
+            obj.value = null;
+        });
+        board.forEach(node => {
+            node.innerHTML = "";
+        })
     };
 
     return {
@@ -108,8 +120,12 @@ const gameBoard = (() => {
         initializeBoard,
         boardState,
         updateBoardState,
-        checkWin
+        checkWin,
+        resetBoard
     }
 })();
+
+const resetButton = document.querySelector(".reset-btn");
+resetButton.addEventListener("click", gameBoard.resetBoard);
 
 window.onload = gameBoard.initializeBoard(gameBoard.board);
